d54499a33369318e38d4af4e0d9e50bb01abb7de
sabir <sksabiruddin81@gmail.com>
2023-07-23 20:44:30 +0530

    submitting code-Sun Jul 23 2023 20:44:23 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,49 @@
-
-import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [progress, setProgress] = useState(0);
 
-export default App
+  useEffect(() => {
+    let intervalId = setInterval(() => {
+      setProgress((prev) => prev + 50);
+    }, 1000);
+
+    return () => {
+      clearTimeout(intervalId);
+    };
+  }, []);
+
+  if (progress > 500) {
+    setProgress(0);
+  }
+
+  return (
+    <div
+      style={{
+        width: "600px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h3>Progress Bar</h3>
+      <div
+        id="barOuter"
+        style={{ width: "500px", height: "15px", backgroundColor: "grey" }}
+      >
+        <div
+          id="barInner"
+          style={{
+            width: `${progress}px`,
+            height: "100%",
+            backgroundColor: "blue",
+          }}
+        ></div>
+      </div>
+      <p>{Math.floor(progress / 5)}%</p>
+    </div>
+  );
+};
+
+export default App;
